387e68c5da18551c9304b6e7a23265f222a96cbb
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 13:06:02 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118056/latihan9/teks/PBOIF210118056Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118056.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * Teks
+ */
+public class PBOIF210118056Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c =  'A';
+        System.out.println("Hasil" + "Char:" + tab + "besiri " + c);
+    }
+    
+}
